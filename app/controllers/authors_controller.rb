b9148bce4280ece3f6f4573855afc8faaea51426
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-10-25 01:47:43 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -11,7 +11,8 @@ class AuthorsController < ApplicationController
     if @author.valid?
       @author.save
       redirect_to author_path(@author)
-    else   
+    else 
+      render
   end
 
   private
